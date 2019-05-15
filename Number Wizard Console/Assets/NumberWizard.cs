172ed9f8e83c9e975fa2da04d0891c05b4ac7bb7
xNiC0 <45330099+xNiC0@users.noreply.github.com>
2019-05-15 20:11:16 +0200

    Basic stuff
    Debug.Log
    Read input (Input.GetKey)
    Retrive keyboard pressed key (KeyCode.value)

---
+++ Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int max = 1000;
+        int min = 1;
+
+        Debug.Log("Welcome to Number Wizard!!!");
+        Debug.Log("Pick a number...");
+        Debug.Log("Highest number is: " + max);
+        Debug.Log("Lowest number is 1: " + min);
+        Debug.Log("Push UP = higher, Push DOWN = Lower, Push ENTER = Correct");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow)) {
+            Debug.Log("UpArrow is pressed");
+        }
+        if(Input.GetKey(KeyCode.DownArrow)) {
+            Debug.Log("DownArrow is pressed");
+        }
+        if(Input.GetKey(KeyCode.Return)) {
+            Debug.Log("Return is pressed");
+        }
+    }
+}

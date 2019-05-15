b1335d01897864b9e756952e14c524db32859870
xNiC0 <45330099+xNiC0@users.noreply.github.com>
2019-05-15 20:50:40 +0200

    Added binary search

--- Number Wizard Console/Assets/NumberWizard.cs
+++ Number Wizard Console/Assets/NumberWizard.cs
@@ -4,30 +4,60 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Un po' di OOP :)
+    private int max;
+    private int min;
+    private int guess;
+
+    public NumberWizard()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+    }
+
+    /*
+     * Start is called before the first frame update
+     * Funzione di inizializzazione
+    */
     void Start()
     {
-        int max = 1000;
-        int min = 1;
-
         Debug.Log("Welcome to Number Wizard!!!");
-        Debug.Log("Pick a number...");
+        Debug.Log("Pick a number and don't tell me what is it...");
         Debug.Log("Highest number is: " + max);
         Debug.Log("Lowest number is 1: " + min);
         Debug.Log("Push UP = higher, Push DOWN = Lower, Push ENTER = Correct");
+        Debug.Log("Your number is " + guess + "?");
     }
 
-    // Update is called once per frame
+    /*
+     * Update is called once per frame
+     * Dipende da quante frame il calcolatore riesce a generare
+     * Se tengo il tasto premuto per 100fps -> stampa 100 volte il messaggio
+    */
     void Update()
     {
+        /*
+         * Usando if in successione posso premere piu' tasti contemporaneamente
+         * Con else if limito ad uno solo tasto
+         * 
+         * Dipende dai casi (multiple press - single press)
+        */
+
         if(Input.GetKey(KeyCode.UpArrow)) {
             Debug.Log("UpArrow is pressed");
+            guess = (min + guess) / 2;
+            Debug.Log("Your number is " + guess + "?");
         }
-        if(Input.GetKey(KeyCode.DownArrow)) {
+        else if(Input.GetKey(KeyCode.DownArrow)) {
             Debug.Log("DownArrow is pressed");
+            guess = (max + guess) / 2;
+            Debug.Log("Your number is " + guess + "?");
         }
-        if(Input.GetKey(KeyCode.Return)) {
+        else if(Input.GetKey(KeyCode.Return)) {
             Debug.Log("Return is pressed");
+            Debug.Log("Aahah i got it ;-\\)");
+            Debug.Log("Your number is " + guess + "?");
         }
     }
 }
